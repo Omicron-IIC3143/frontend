940d3c6c7d7fd155e489831aefc97a3f5e0b4746
Martin Sanchez <msanchez9@uc.cl>
2022-05-02 12:18:15 -0400

    Navbar almost ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 import Navbar from './app/redux/components/navbar/Navbar';
+import { ProjectList } from './app/redux/components/projectList/ProjectList';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import './App.css';
@@ -10,6 +11,9 @@ function App() {
     <Router>
         <div className="App">
           <Navbar />
+        </div>
+        <div>
+          <ProjectList />
         </div>
     </Router>
   );
