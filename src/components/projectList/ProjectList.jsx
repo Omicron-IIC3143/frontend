999cb9fb5c9b0b7eb864b6396ec8fb7c2ab91a37
Ismael Contreras Morgado <icontreras2@uc.cl>
2022-05-29 20:43:08 -0400

    Feat(projectShow): Addinf components of of view of projectShow

--- src/components/projectList/ProjectList.jsx
+++ src/components/projectList/ProjectList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+// import { Link } from 'react-router-dom';
 
 
-export function ProjectList({type, title, description, daysAgo}) {
+export function ProjectList({id, type, title, description, daysAgo}) {
     return (
         <Card className="text-center">
         <Card.Header> Proyecto de {type} </Card.Header>
@@ -12,7 +13,8 @@ export function ProjectList({type, title, description, daysAgo}) {
             <Card.Text>
                 {description}
             </Card.Text>
-            <Button variant="primary">Ver en más detalle</Button>
+            <Button variant="primary" href={`/projects/${id}`}>Ver en más detalle</Button>
+            {/* <Link to={`/projects/${id}`}>Ver en más detalle</Link> */}
         </Card.Body>
         <Card.Footer className="text-muted">Hace {daysAgo} días</Card.Footer>
         </Card>
