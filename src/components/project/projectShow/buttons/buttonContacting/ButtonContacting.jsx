c9b65c657282f8913ad4535d3f722192002822ce
felipegarcia98 <fagarcia5@uc.cl>
2022-06-22 00:38:53 -0400

    feat(button contact project): Added contact href

--- src/components/project/projectShow/buttons/buttonContacting/ButtonContacting.jsx
+++ src/components/project/projectShow/buttons/buttonContacting/ButtonContacting.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import './ButtonContacting.css';
 
-function ButtonContacting() {
+function ButtonContacting({ visitUser, project, projectUser }) {
+  let user = visitUser;
+  if (!visitUser) { user = { name: 'Usuario Visitante' }; }
   return (
-    <Button variant="primary" className="contactingButton">
+    <Button variant="primary" className="contactingButton" href={`mailto:${projectUser.email}?subject=Social%20Starter%20-%20Contacto%20por%20Proyecto%20${project.name}%20-%20Usuario%20${user.name}`}>
       {/* startIcon={<DeleteIcon />} */}
       Contactar Proyecto
     </Button>
