3a9a12d6263de4b4d5e3f5644827ddc9227aa7ab
icontreras2 <44145317+icontreras2@users.noreply.github.com>
2022-06-23 16:02:24 -0400

    Loading financed projects view (#77)
    * Add: case in my funded projects when user does not have transactions of financing yet (incompleted)
    
    * Add: case in my funded projects when user does not have transactions of financing yet (completed)
    
    * Add: case in my funded projects when user does not have transactions of financing yet (completed with 0 errors in ESLint)
    
    * Add(link): Add link to userShowView from MyFundedProjects view

--- src/views/project/myFunded/MyFunded.jsx
+++ src/views/project/myFunded/MyFunded.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useParams, NavLink } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
 import Navbar from '../../../components/navbar/Navbar';
 import ButtonPostulate from '../../../components/project/projectList/buttonPostulateProject/ButtonPostulateProject';
@@ -7,12 +8,14 @@ import '../../landingPage/LandingPage.css';
 import ButtonBack from '../../../components/buttons/buttonBack/ButtonBack';
 import extractFundedProjects from '../../../hooks/finances';
 import Loading from '../../../components/loading/Loading';
+import './MyFunded.css';
 
 function MyProjects() {
   const { currentUser } = useAuth();
   // const [projects, setProjects] = useState([]);
   const [finances, setFinances] = useState([]);
   const [loading, setLoading] = useState(false);
+  const { id } = useParams();
   // eslint-disable-next-line no-unused-vars
   const [error, setError] = useState(false);
 
@@ -25,72 +28,103 @@ function MyProjects() {
         Authorization: `Bearer ${currentUser?.token}`,
       },
     };
-
-    const fetchData = (url) => fetch(url, requestOptions).then((r) => r.json());
-
-    const [projectsData, financesData] = await Promise.all([
-      fetchData(`${process.env.REACT_APP_API_URL}/projects`),
-      fetchData(`${process.env.REACT_APP_API_URL}/finance/transactions/${currentUser.id}`),
-    ]);
-    const filter = extractFundedProjects(projectsData, financesData);
-    setLoading(false);
-    setFinances(filter);
+    let filter = [];
+    try {
+      const fetchData = (url) => fetch(url, requestOptions).then((r) => r.json());
+      const [projectsData, financesData] = await Promise.all([
+        fetchData(`${process.env.REACT_APP_API_URL}/projects`),
+        fetchData(`${process.env.REACT_APP_API_URL}/finance/transactions/${id}`),
+      ]);
+      filter = extractFundedProjects(projectsData, financesData);
+    } catch (errorCatch) {
+      setError(errorCatch);
+    } finally {
+      setLoading(false);
+      setFinances(filter);
+    }
   };
 
   useEffect(() => {
     getData();
   }, []);
 
-  if (loading) {
-    return (
-      <Loading />
+  if (currentUser?.isAdmin || currentUser?.id == id) {
+    if (loading) {
+      return (
+        <Loading />
+      );
+    } return (
+      <div className="grid-container">
+        <div>
+          <Navbar />
+        </div>
+        <div className="flex-landing-page">
+
+          {currentUser?.id != id ? (
+            <h1>
+              Proyectos financiados por el
+              {' '}
+              {' '}
+              <NavLink exact to={`/users/${id}`} activeClassName="activeClicked" className="color-link">
+                {`usuario de id ${id}`}
+              </NavLink>
+            </h1>
+          ) : (
+            <>
+              <h1>Proyectos financiados por mí</h1>
+              <div className="flex-inside-button-postulate">
+                <ButtonPostulate />
+              </div>
+            </>
+          )}
+          {error ? (
+            <div className="flex-inside">
+              <h2>
+                {error.errors}
+              </h2>
+            </div>
+          ) : (
+            <> </>
+          )}
+
+          {finances.length === 0 ? (
+            <h4>
+              No hay proyectos financiados.
+            </h4>
+
+          ) : (
+            finances.map((project) => (
+              // acá hay que poner (project?.currentState == 'approved') ? (
+              (project?.currentState == 'accepted') ? (
+                <div className="flex-inside">
+                  <ProjectList
+                    id={project?.id}
+                    topic={project?.topic}
+                    title={project?.name}
+                    description={project?.description}
+                    date={project?.createdAt}
+                    company={project?.company}
+                  />
+                </div>
+              ) : (
+                <>
+                </>
+              )
+            ))
+          )}
+          <div>
+            <ButtonBack />
+          </div>
+        </div>
+      </div>
     );
-  } return (
+  }
+  return (
     <div className="grid-container">
       <div>
         <Navbar />
       </div>
-      <div className="flex-landing-page">
-        <h1>Mis proyectos financiados</h1>
-        {currentUser ? (
-          <div className="flex-inside-button-postulate">
-            <ButtonPostulate />
-          </div>
-        ) : (
-          <>
-          </>
-        ) }
-        {error ? (
-          <div className="flex-inside">
-            <h2>
-              No hay proyectos aún.
-              {error.errors}
-            </h2>
-          </div>
-        ) : (
-          finances.map((project) => (
-            // acá hay que poner (project?.currentState == 'approved') ? (
-            (project?.currentState == 'accepted') ? (
-              <div className="flex-inside">
-                <ProjectList
-                  id={project?.id}
-                  topic={project?.topic}
-                  title={project?.name}
-                  description={project?.description}
-                  date={project?.createdAt}
-                  company={project?.company}
-                />
-              </div>
-            ) : (
-              <>
-              </>
-            )
-          ))
-        )}
-        <div>
-          <ButtonBack />
-        </div>
-      </div>
+      <h1 className="unauthorizedMessageMyProjects">No estás autorizado para ver los proyectos financiados por otro usuario. </h1>
     </div>
   );
 }
