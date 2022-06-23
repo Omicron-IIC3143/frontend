29a9454c4374755b8e0ba6d9d9f21602145eb3d6
Ismael Contreras Morgado <icontreras2@uc.cl>
2022-06-22 20:16:26 -0400

    Add: case in my funded projects when user does not have transactions of financing yet (incompleted)

--- src/hooks/finances.js
+++ src/hooks/finances.js
@@ -1,7 +1,11 @@
 function extractFundedProjects(projects, finances) {
+  console.log(finances);
+  if (finances.length === 0) {
+    return []
+  }
   const projectsId = finances.map((finance) => finance.projectId);
   const projectsFilter = projects.filter(({ id }) => projectsId.includes(id));
-
+  
   return projectsFilter;
 }
 
--- src/views/project/myFunded/MyFunded.jsx
+++ src/views/project/myFunded/MyFunded.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
 import Navbar from '../../../components/navbar/Navbar';
 import ButtonPostulate from '../../../components/project/projectList/buttonPostulateProject/ButtonPostulateProject';
@@ -13,6 +14,7 @@ function MyProjects() {
   // const [projects, setProjects] = useState([]);
   const [finances, setFinances] = useState([]);
   const [loading, setLoading] = useState(false);
+  const { id } = useParams();
   // eslint-disable-next-line no-unused-vars
   const [error, setError] = useState(false);
 
@@ -27,11 +29,13 @@ function MyProjects() {
     };
 
     const fetchData = (url) => fetch(url, requestOptions).then((r) => r.json());
-
+    // acá se queda cuando el usuario aún no ha financiado proyectos
+    console.log("antes");
     const [projectsData, financesData] = await Promise.all([
       fetchData(`${process.env.REACT_APP_API_URL}/projects`),
-      fetchData(`${process.env.REACT_APP_API_URL}/finance/transactions/${currentUser.id}`),
+      fetchData(`${process.env.REACT_APP_API_URL}/finance/transactions/${id}`),
     ]);
+    console.log("después");
     const filter = extractFundedProjects(projectsData, financesData);
     setLoading(false);
     setFinances(filter);
@@ -41,56 +45,78 @@ function MyProjects() {
     getData();
   }, []);
 
-  if (loading) {
-    return (
-      <Loading />
+  if (currentUser?.isAdmin || currentUser?.id == id) {
+    if (loading) {
+      return (
+        <Loading />
+      );
+    } return (
+      <div className="grid-container">
+        <div>
+          <Navbar />
+        </div>
+        <div className="flex-landing-page">
+
+          {currentUser?.id != id ? (
+            <h1>Proyectos financiados por otro usuario</h1>
+          ) : (
+            <>
+              <h1>Proyectos financiados por mí</h1>
+              <div className="flex-inside-button-postulate">
+                <ButtonPostulate />
+              </div>
+            </>
+          )}
+          {error ? (
+            <div className="flex-inside">
+              <h2>
+                No hay proyectos aún.
+                {error.errors}
+              </h2>
+            </div>
+          ) : (
+            <> </>
+          )}
+
+          
+          {finances.length===0 ? (
+            <h4>
+              Aún no has financiado proyectos.
+            </h4>
+
+          ) : (
+              finances.map((project) => (
+                // acá hay que poner (project?.currentState == 'approved') ? (
+                (project?.currentState == 'accepted') ? (
+                  <div className="flex-inside">
+                    <ProjectList
+                      id={project?.id}
+                      topic={project?.topic}
+                      title={project?.name}
+                      description={project?.description}
+                      date={project?.createdAt}
+                      company={project?.company}
+                    />
+                  </div>
+                ) : (
+                  <>
+                  </>
+                )
+              ))
+          )}
+          <div>
+            <ButtonBack />
+          </div>
+        </div>
+      </div>
     );
-  } return (
+  }
+  return (
     <div className="grid-container">
       <div>
         <Navbar />
       </div>
-      <div className="flex-landing-page">
-        <h1>Mis proyectos financiados</h1>
-        {currentUser ? (
-          <div className="flex-inside-button-postulate">
-            <ButtonPostulate />
-          </div>
-        ) : (
-          <>
-          </>
-        ) }
-        {error ? (
-          <div className="flex-inside">
-            <h2>
-              No hay proyectos aún.
-              {error.errors}
-            </h2>
-          </div>
-        ) : (
-          finances.map((project) => (
-            // acá hay que poner (project?.currentState == 'approved') ? (
-            (project?.currentState == 'accepted') ? (
-              <div className="flex-inside">
-                <ProjectList
-                  id={project?.id}
-                  topic={project?.topic}
-                  title={project?.name}
-                  description={project?.description}
-                  date={project?.createdAt}
-                  company={project?.company}
-                />
-              </div>
-            ) : (
-              <>
-              </>
-            )
-          ))
-        )}
-        <div>
-          <ButtonBack />
-        </div>
-      </div>
+      <h1 className="unauthorizedMessageMyProjects">No estás autorizado para ver los proyectos financiados por otro usuario. </h1>
     </div>
   );
 }
