e4290603b3958f87430ce172ab56c12449bb528d
Felipe García González <fagarcia5@uc.cl>
2022-06-23 20:59:58 -0400

    Fix/select 2 (#78)
    * fix(register project): Fixed multi select
    
    * feat(register project): Set message before alert
    
    * lint: Fixed register project eslint fails

--- src/components/project/registerProject/RegisterProjectForm.jsx
+++ src/components/project/registerProject/RegisterProjectForm.jsx
@@ -1,13 +1,20 @@
+/* eslint-disable no-alert */
 import React, { useState } from 'react';
 import { Formik, Form, Field } from 'formik';
 import { useNavigate } from 'react-router-dom';
+import Select from 'react-select';
 import * as Yup from 'yup';
 import Button from 'react-bootstrap/Button';
 import useAuth from '../../../hooks/useAuth';
 import stringDateOfTomorrow from '../../../hooks/stringDateOfTomorrow';
 import './RegisterProjectForm.css';
 import ButtonBack from '../../buttons/buttonBack/ButtonBack';
-import SelectComponent from './SelectComponent';
+import options from './SelectOptions';
+
+/*
+Para ayudarse con alguna modificación del Select
+Source: https://gist.github.com/hubgit/e394e9be07d95cd5e774989178139ae8?permalink_comment_id=3487405#gistcomment-3487405
+*/
 
 function RegisterProjectForm() {
   const navigate = useNavigate();
@@ -31,8 +38,8 @@ function RegisterProjectForm() {
       .required('El nombre de la empresa, fundación u organización es obligatorio.'),
     topic: Yup.string()
       .required('El rubro relacionado al proyecto es obligatorio.'),
-    // tags: Yup.string()
-    //   .required('Este campo es obligatorio.'),
+    tags: Yup.array()
+      .min(1, 'Debes seleccionar al menos un tag'),
     goalAmount: Yup.number()
       .required('La meta monetaria es obligatoria.'),
     date: Yup.date()
@@ -43,13 +50,14 @@ function RegisterProjectForm() {
   return (
     <div className="form center-content-x width-100">
       <Formik
+        enableReinitialize
         initialValues={{
           name: '',
           description: '',
           pictureUrl: '',
           company: '',
           topic: '',
-          tags: '',
+          tags: [],
           goalAmount: '',
           date: '',
         }}
@@ -60,12 +68,15 @@ function RegisterProjectForm() {
             userId: currentUser.id,
             currentAmount: 0,
           };
+          newValues.tags = values.tags.map((item) => item.value);
           setLoading(true);
           const requestOptions = {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
               Authorization: `Bearer ${currentUser?.token}`,
+              // 'Access-Control-Allow-Origin': `${window.location.hostname}`,
+              // 'Sec-Fetch-Mode': 'no-cors',
               'Sec-Fetch-Mode': 'cors',
             },
             body: JSON.stringify(newValues),
@@ -77,6 +88,7 @@ function RegisterProjectForm() {
               throw new Error(error);
             }
             setMessage('Postulación creada correctamente.');
+            alert('Postulación creada correctamente.');
             navigate(`/users/${currentUser.id}/projects`);
           } catch (error) {
             setMessage(error.message);
@@ -86,85 +98,108 @@ function RegisterProjectForm() {
         }}
 
       >
-        {({ errors, touched }) => (
-          <div className="card-profile-register-project-form width-80">
-            <Form>
-              <div className="label-form-register-project">
-                <label className="label-content-register-project" htmlFor="name">Nombre del proyecto: </label>
-                <Field className="center-info-register-project" name="name" type="text" placeholder="Nombre proyecto" />
-                {errors.name && touched.name && (
-                  <div className="validation-error-register-project">{errors.name}</div>
-                )}
-              </div>
-
-              <div className="label-form-register-project">
-                <label className="label-content-register-project-description" htmlFor="description">Descripción: </label>
-                <Field className="center-info-register-project-description" as="textarea" name="description" type="text" placeholder="¿En qué consiste, qué busca?" />
-                {errors.description && touched.description && (
-                  <div className="validation-error-register-project">{errors.description}</div>
-                )}
-              </div>
-
-              <div className="label-form-register-project">
-                <label className="label-content-register-project" htmlFor="pictureUrl">Imagen: </label>
-                <Field className="center-info-register-project" name="pictureUrl" type="url" placeholder="URL de la imagen" />
-                {errors.pictureUrl && touched.pictureUrl && (
-                  <div className="validation-error-register-project">{errors.pictureUrl}</div>
-                )}
-              </div>
-
-              <div className="label-form-register-project">
-                <label className="label-content-register-project" htmlFor="company">Empresa: </label>
-                <Field className="center-info-register-project" name="company" type="text" placeholder="Empresa, fundación u organización" />
-                {errors.company && touched.company && (
-                  <div className="validation-error-register-project">{errors.company}</div>
-                )}
-              </div>
-
-              <div className="label-form-register-project">
-                <label className="label-content-register-project" htmlFor="topic">Rubro: </label>
-                <Field className="center-info-register-project" name="topic" type="text" placeholder="Área relacionada con la causa del proyecto" />
-                {errors.topic && touched.topic && (
-                  <div className="validation-error-register-project">{errors.topic}</div>
-                )}
-              </div>
-
-              <div className="label-form-register-project">
-                <label className="label-content-register-project" htmlFor="tags">Tags: </label>
-                <Field as={SelectComponent} name="tags" />
-                {errors.tags && touched.tags && (
-                  <div className="validation-error-register-project">{errors.tags}</div>
-                )}
-              </div>
-
-              <div className="label-form-register-project">
-                <label className="label-content-register-project" htmlFor="goalAmount">Meta monetaria: </label>
-                <Field className="center-info-register-project" name="goalAmount" type="number" placeholder="CLP" />
-                {errors.goalAmount && touched.goalAmount && (
-                  <div className="validation-error-register-project">{errors.goalAmount}</div>
-                )}
-              </div>
-
-              <div className="label-form-register-project">
-                <label className="label-content-register-project" htmlFor="date">Tiempo límite: </label>
-                <Field className="center-info-register-project" name="date" type="date" placeholder="Cantidad de días desde hoy" />
-                {errors.date && touched.date && (
-                  <div className="validation-error-register-project">{errors.date}</div>
-                )}
-              </div>
-              {!loading ? (
-                <div className="label-form-register-project button-submit-register-project page-buttons margin-bottom-s">
-                  <ButtonBack />
-                  <Button variant="primary" type="submit">Enviar postulación</Button>
+        {(kwargs) => {
+          const {
+            errors,
+            touched,
+            values,
+            handleSubmit,
+            handleBlur,
+            setFieldValue,
+          } = kwargs;
+          return (
+            <div className="card-profile-register-project-form width-80 margin-bottom-m">
+              <Form onSubmit={handleSubmit}>
+                <div className="label-form-register-project">
+                  <label className="label-content-register-project" htmlFor="name">Nombre del proyecto: </label>
+                  <Field className="center-info-register-project" name="name" type="text" placeholder="Nombre proyecto" />
+                  {errors.name && touched.name && (
+                    <div className="validation-error-register-project">{errors.name}</div>
+                  )}
                 </div>
-              ) : (
-                <div>
-                  <p className="final-message-form-user">Postulando proyecto...</p>
+
+                <div className="label-form-register-project">
+                  <label className="label-content-register-project-description" htmlFor="description">Descripción: </label>
+                  <Field className="center-info-register-project-description" as="textarea" name="description" type="text" placeholder="¿En qué consiste, qué busca?" />
+                  {errors.description && touched.description && (
+                    <div className="validation-error-register-project">{errors.description}</div>
+                  )}
                 </div>
-              )}
-            </Form>
-          </div>
-        )}
+
+                <div className="label-form-register-project">
+                  <label className="label-content-register-project" htmlFor="pictureUrl">Imagen: </label>
+                  <Field className="center-info-register-project" name="pictureUrl" type="url" placeholder="URL de la imagen" />
+                  {errors.pictureUrl && touched.pictureUrl && (
+                    <div className="validation-error-register-project">{errors.pictureUrl}</div>
+                  )}
+                </div>
+
+                <div className="label-form-register-project">
+                  <label className="label-content-register-project" htmlFor="company">Empresa: </label>
+                  <Field className="center-info-register-project" name="company" type="text" placeholder="Empresa, fundación u organización" />
+                  {errors.company && touched.company && (
+                    <div className="validation-error-register-project">{errors.company}</div>
+                  )}
+                </div>
+
+                <div className="label-form-register-project">
+                  <label className="label-content-register-project" htmlFor="topic">Rubro: </label>
+                  <Field className="center-info-register-project" name="topic" type="text" placeholder="Área relacionada con la causa del proyecto" />
+                  {errors.topic && touched.topic && (
+                    <div className="validation-error-register-project">{errors.topic}</div>
+                  )}
+                </div>
+
+                <div className="label-form-register-project">
+                  <label className="label-content-register-project" htmlFor="tags">Tags: </label>
+                  <Select
+                    isMulti
+                    id="tags"
+                    name="tags"
+                    className="font-size-2"
+                    type="text"
+                    value={values.tags}
+                    onBlur={handleBlur}
+                    options={options}
+                    placeholder="Presiona para agregar tags y en la 'x' para deseleccionar"
+                    onChange={(option) => {
+                      const optionsTags = [...values.tags];
+                      if (!(option in optionsTags)) { optionsTags.push(option); }
+                      setFieldValue('tags', option);
+                    }}
+                  />
+                  {errors.tags && touched.tags && (
+                    <div className="validation-error-register-project">{errors.tags}</div>
+                  )}
+                </div>
+                <div className="label-form-register-project">
+                  <label className="label-content-register-project" htmlFor="goalAmount">Meta monetaria: </label>
+                  <Field className="center-info-register-project" name="goalAmount" type="number" placeholder="CLP" />
+                  {errors.goalAmount && touched.goalAmount && (
+                    <div className="validation-error-register-project">{errors.goalAmount}</div>
+                  )}
+                </div>
+                <div className="label-form-register-project">
+                  <label className="label-content-register-project" htmlFor="date">Tiempo límite: </label>
+                  <Field className="center-info-register-project" name="date" type="date" placeholder="Cantidad de días desde hoy" />
+                  {errors.date && touched.date && (
+                    <div className="validation-error-register-project">{errors.date}</div>
+                  )}
+                </div>
+                {!loading ? (
+                  <div className="label-form-register-project button-submit-register-project page-buttons margin-bottom-s">
+                    <ButtonBack />
+                    <Button variant="primary" type="submit">Enviar postulación</Button>
+                  </div>
+                ) : (
+                  <div>
+                    <p className="final-message-form-user">Postulando proyecto...</p>
+                  </div>
+                )}
+              </Form>
+            </div>
+          );
+        }}
       </Formik>
       <p>{message}</p>
     </div>
