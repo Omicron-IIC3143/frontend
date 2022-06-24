af6c57ff1bf2870f90ba2216f9b9ec7308103d61
absoto4 <absoto4@uc.cl>
2022-06-24 01:59:22 -0400

    Reset commit

--- src/components/buttons/__tests__/ButtonBack.test.jsx
+++ src/components/buttons/__tests__/ButtonBack.test.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import ButtonBack from '../buttonBack/ButtonBack';
 
 test('Renders login title', () => {
+  // Test commit
   const { getByText } = render(
     <Router>
       <ButtonBack />
