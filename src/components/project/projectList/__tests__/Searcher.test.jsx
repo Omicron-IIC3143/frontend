c4fd4cc7c59e11de4e74ab6e3562cd9f6676c526
smatus9 <smatus@uc.cl>
2022-06-24 05:30:12 -0400

    first try to add testing to pipeline

--- src/components/project/projectList/__tests__/Searcher.test.jsx
+++ src/components/project/projectList/__tests__/Searcher.test.jsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import Searcher from '../searcher/Searcher';
 
 test('Renders login title', () => {
-  const { container, getByPlaceholderText } = render(<Searcher />);
-  expect(getByPlaceholderText('Buscar Proyecto')).toBeInTheDocument();
+  const { container, getByText } = render(<Searcher className="col-stock-sale" />);
+  expect(getByText('Filtrar')).toBeInTheDocument();
   expect(container.firstChild).toHaveClass('col-stock-sale');
 });
