e73432ab114dfa24b8c042f8763ea162a444f3bb
smatus9 <smatus@uc.cl>
2022-06-24 04:20:12 -0400

    test routes added to config

--- src/components/project/projectShow/__tests__/ButtonSharing.test.jsx
+++ src/components/project/projectShow/__tests__/ButtonSharing.test.jsx
@@ -4,6 +4,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import ButtonSharing from '../buttons/buttonSharing/ButtonSharing';
 
 test('Renders login title', () => {
+  window.location.href = 'http://localhost:3000/project/1';
+  const { getByText } = render(
+    <Router>
+      <ButtonSharing />
+    </Router>,
+  );
+  getByText('Compartir Proyecto');
+});
+test('onClick correct', () => {
+  // window.location.href = 'http://localhost:3000/project/1';
   const { getByText } = render(
     <Router>
       <ButtonSharing />
