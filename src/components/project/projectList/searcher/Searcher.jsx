9a17a41dfae67038016bbfade25d42408157dbb0
goyo20 <53911624+goyo20@users.noreply.github.com>
2022-06-23 23:39:18 -0400

    Search tags (#81)
    * pequeño cambio de estilo
    
    * Update search functionality
    
    Co-authored-by: Ismael Contreras Morgado <icontreras2@uc.cl>

--- src/components/project/projectList/searcher/Searcher.jsx
+++ src/components/project/projectList/searcher/Searcher.jsx
@@ -7,6 +7,18 @@ const { Search } = Input;
 
 function Searcher(data) {
   const { projects, filterData, setFilterData } = data;
+
+  // const filtro = (dataProject, valor) => {
+  //   console.log(dataProject);
+  //   const lower = dataProject.map(({ tags }) => tags.map((tag) => tag.toLowerCase));
+  //   console.log(lower);
+  //   const filtrado = lower.filter(({ tags }) => tags.includes(valor.toLowerCase()));
+  //   console.log(filtrado);
+  //   return filtrado;
+  // };
+
+  // projects.filter(({ tags }) => tags.toLowerCase().includes(value.toLowerCase())
+
   const onSearch = (value) => {
     // console.log('hola');
     // console.log(value);
@@ -14,19 +26,22 @@ function Searcher(data) {
     if (
       filterData
       // eslint-disable-next-line react/destructuring-assignment
-      !== projects.filter(({ tags }) => tags.toLowerCase().includes(value.toLowerCase()))
+      !== projects.filter(({ tags }) => tags.includes(value.toLowerCase()))
     ) {
       // eslint-disable-next-line react/destructuring-assignment
       // eslint-disable-next-line max-len
-      const filter = projects.filter(({ tags }) => tags.toLowerCase().includes(value.toLowerCase()));
+      const filter = projects.filter(({ tags }) => tags.includes(value.toLowerCase()));
       setFilterData(filter);
+    }
+    if (value == '') {
+      setFilterData(projects);
     }
   };
   return (
     <Search
       className="col-stock-sale"
       placeholder="Buscar Proyecto"
-      onChange={onSearch}
+      onSearch={onSearch}
       // esto hay q revisar que funcione.
     />
   );
